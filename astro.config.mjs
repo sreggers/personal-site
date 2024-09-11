fa20b6409e7f7017ff6982ce93eb033547974cc9
Sebastiaan <sebastiaan.reggers@jcore.com>
2024-09-11 22:14:29 +0200

    Deploy to Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sreggers.github.io',
+  base: 'personal-site',
   i18n: {
     defaultLocale: "en",
     locales: ["en", "de", "nl"],
